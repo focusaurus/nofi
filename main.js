cb6533b3b20e99b34d8f2de2f9629cc3a8bc110a
Peter Lyons <pete@peterlyons.com>
2023-08-25 06:19:14 -0400

    sketch out basic modal menu and console keystroke handling

---
+++ main.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+const path = require("node:path");
+const childProcess = require("node:child_process");
+require("keypress")(process.stdin);
+// without this, we would only get streams once enter is pressed
+process.stdin.setRawMode(true);
+
+// resume stdin in the parent process (node app won't quit all by itself
+// unless an error or process.exit() happens)
+process.stdin.resume();
+
+process.stdin.setEncoding("utf8");
+
+function printMenu(menu) {
+  Object.entries(menu).forEach(([key, action]) => {
+    console.log(`${key}: ${action.label}`);
+  });
+}
+
+let configPath = process.argv[2];
+if (!configPath.startsWith("/")) {
+  configPath = path.join(process.cwd(), configPath);
+}
+// console.log(configPath);
+
+const menu = require(configPath);
+
+printMenu(menu);
+let menuStack = [menu];
+process.stdin.on("keypress", function (ch, key) {
+  if (key && key.ctrl && key.name === "c") {
+    process.stdin.pause();
+    return;
+  }
+
+  const mode = menuStack[0];
+  const action = mode[ch];
+  if (!action) {
+    console.log(`Nothing bound to ${ch}`);
+    return;
+  }
+  if (action.command) {
+    console.log(`🚀 ${action.command.join(" ")}`);
+    childProcess.spawn(action.command[0], action.command.slice(1));
+    // re-initialize
+    menuStack = [menu];
+    console.clear();
+    printMenu(menu);
+    return;
+  }
+  if (action.menu) {
+    menuStack.unshift(action.menu);
+    printMenu(action.menu);
+  }
+});
